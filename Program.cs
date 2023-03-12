67865403567a5671a00a19d7f0d9c202417b489e
dev-xpeace <xpeacedev@outlook.com>
2023-03-13 01:44:10 +0300

    cubeCalculator
    Basic cube calculator with method

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+int cubedNumber = cube(num);
+Console.WriteLine(cubedNumber);
+
+Console.ReadLine();
+
+
+static int cube(int num)
+{
+    int result = (num * num * num);
+    return result;
+}
